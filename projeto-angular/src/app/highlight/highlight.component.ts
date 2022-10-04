ba68f5edf98f723ae9cc353fa2b1f26a9aadd90f
João Ricardo Tasca Puel <jrtpuel@furb.br>
2022-10-03 21:40:08 -0300

    Aula_03_10

---
+++ projeto-angular/src/app/highlight/highlight.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-highlight',
+  templateUrl: './highlight.component.html',
+  styleUrls: ['./highlight.component.css']
+})
+export class HighlightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  title = 'projeto-angular';
+}
