efed836bda86a53fb29b9171a1602238860ea85a
João Ricardo Tasca Puel <jrtpuel@furb.br>
2022-10-04 21:32:20 -0300

    Aula_04_10

--- projeto-angular/src/app/app.module.ts
+++ projeto-angular/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { HeaderComponent } from './header/header.component';
 import { HighlightComponent } from './highlight/highlight.component';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { HighlightComponent } from './highlight/highlight.component';
     HighlightComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
